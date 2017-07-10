425d7ea6a205666b1530df3e7e8d08706d52d979
Jason Ainsworth and Nick Powell <nick@>
2017-07-10 15:33:07 -0700

    add cart and products buttonz

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,10 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
     @order_item = current_order.order_items.new
   end
 
