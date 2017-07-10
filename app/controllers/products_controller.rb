36c823236c71dc136a842b3fbdfaed0ddbb0d08a
Jason Ainsworth and Nick Powell <jason+nick@>
2017-07-10 14:36:26 -0700

    add order_items_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,17 @@
 class ProductsController < ApplicationController
 
-    def index
-      @products = Product.all
-    end
+  def index
+    @products = Product.all
+    @order_item = current_order.order_items.new
+  end
+
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :category, :description)
+  end
+
 
 end
