49e82194efba63df520154ac45bbf3eacf90aeaf
Jason Ainsworth and Nick Powell <jason@>
2017-07-12 10:31:35 -0700

    add in confirmable email option through devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_one :account
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   after_create :save_account
 
