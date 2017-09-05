dffc50ce3c80793275ef560425a5eba8681b9cdd
Jason Ainsworth <jasoncruze@gmail.com>
2017-09-04 17:19:50 -0700

    change tp product image

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 
 Product.destroy_all
 
-images_array = ['app/assets/images/beer.jpg', 'app/assets/images/cola.jpg', 'app/assets/images/tp.jpg']
+images_array = ['app/assets/images/beer.jpg', 'app/assets/images/product.jpg', 'app/assets/images/tp.jpg']
 
-50.times do |index|
+20.times do |index|
   image = images_array.shuffle.sample
   Product.create!(name: Faker::Commerce.product_name,
                         price: Faker::Commerce.price,
